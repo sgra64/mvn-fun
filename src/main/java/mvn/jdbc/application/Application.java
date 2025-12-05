d4671190517cb721df303909cb8d15f0b860dbdf
Sven Graupner <sgraupner@bht-berlin.de>
2025-12-05 11:04:35 +0100

    add src/.../Application.java

---
+++ src/main/java/mvn/jdbc/application/Application.java
@@ -0,0 +1,12 @@
+package mvn.jdbc.application;
+
+/**
+ * Run the program:
+ * - mvn exec:java -q
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
